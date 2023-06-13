e5ef9206f521e53464cbebfd3e8b7b951f55d84f
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-06-13 15:20:12 -0600

    update to gpt 16k

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -66,7 +66,7 @@ export const POST: RequestHandler = async ({ request }) => {
     ];
 
     const chatRequestOpts: CreateChatCompletionRequest = {
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-3.5-turbo-16k',
       messages,
       temperature: 0.9,
       stream: true
